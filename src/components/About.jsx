6e12dec7d29c6fd92171425a6e45f19120386b9d
Navodya16 <prabuddhi.rath2@gmail.com>
2024-03-07 13:00:57 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full min-h-screen bg-gradient-to-b from-gray-800 to-black text-white "
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+        <p className="text-xl mt-10">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque
+          consequuntur labore nemo ullam eos soluta qui, architecto placeat
+          dolore exercitationem odio maxime harum eaque ipsa, unde molestias
+          sunt tempore ab culpa quia explicabo ad in eveniet! Quisquam quaerat
+          iste veniam reprehenderit laborum quibusdam hic, eligendi deleniti
+          aliquam distinctio itaque rem!
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          dolorum quis voluptatum sed qui obcaecati nulla pariatur labore
+          tempora deserunt, repellendus ipsam in voluptatibus? Sunt aliquid sit
+          magni? Quas quo debitis quam architecto culpa expedita repellendus
+          quod! Adipisci dolorem totam magnam deleniti, consequatur quae
+          exercitationem, eaque quisquam doloremque earum vero!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Home.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import HeroImage from "../assets/heroImage.jpg";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+
+const Home = () => {
+  return (
+    <div
+      name="home"
+      className="min-h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 flex flex-col items-center justify-center"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center px-4 md:flex-row">
+        <div className="flex flex-col justify-center ">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+            I am a full stack developer
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md">
+            i have 4 years of experience building and designing software
+            currently, i love to work on web applications using technologies
+            like react, php.
+          </p>
+          <div>
+            <button className="group text-white w-fit px-6 py-3 my-2 sm:my-1 mb-2 sm:mb-0 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500">
+              Portforlio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </button>
+          </div>
+        </div>
+        <div>
+          <img
+            src={HeroImage}
+            alt="my profile"
+            className="rounded-2xl mx-auto w-2/3 md:w-full mt-2 sm:mt-0"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
