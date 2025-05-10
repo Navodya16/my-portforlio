f73f74d9902b422492eb0cbdf03b24562b055eaa
Navodya16 <prabuddhi.rath2@gmail.com>
2025-05-10 16:24:35 +0530

    modified experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 
 const Experience = () => {
+  const experiences = [
+    {
+      id: 1,
+      company: "Faculty of Engineering, University of Sri Jayewardenepura",
+      position: "Research Assistant",
+      period: "Sep 2024 - Present",
+      location: "Colombo, Sri Lanka",
+      summary:
+        "Works in research projects related to Machine Learning, Image Processing and Computer Vision.",
+    },
+    {
+      id: 2,
+      company: "Synopsys Lanka (Pvt) Ltd",
+      position: "Research & Development Intern",
+      period: "Feb 2023 - Aug 2023",
+      location: "Colombo, Sri Lanka",
+      summary:
+        "Worked as a R&D intern in the Zebu Front-end team to test and debug issues in the HDL designs",
+    },
+  ];
+
   return (
     <div
       name="Experience"
-      className="w-full min-h-screen bg-gradient-to-b from-gray-800 to-black text-white "
+      className="w-full min-h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
@@ -12,25 +33,23 @@ const Experience = () => {
             Experience
           </p>
         </div>
-        <p className="text-xl mt-10">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque
-          consequuntur labore nemo ullam eos soluta qui, architecto placeat
-          dolore exercitationem odio maxime harum eaque ipsa, unde molestias
-          sunt tempore ab culpa quia explicabo ad in eveniet! Quisquam quaerat
-          iste veniam reprehenderit laborum quibusdam hic, eligendi deleniti
-          aliquam distinctio itaque rem!
-        </p>
 
-        <br />
-
-        <p className="text-xl">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
-          dolorum quis voluptatum sed qui obcaecati nulla pariatur labore
-          tempora deserunt, repellendus ipsam in voluptatibus? Sunt aliquid sit
-          magni? Quas quo debitis quam architecto culpa expedita repellendus
-          quod! Adipisci dolorem totam magnam deleniti, consequatur quae
-          exercitationem, eaque quisquam doloremque earum vero!
-        </p>
+        <div className="space-y-8">
+          {experiences.map(
+            ({ id, company, position, period, location, summary }) => (
+              <div key={id} className="bg-gray-900 p-6 rounded-lg shadow-md">
+                <h3 className="text-2xl font-semibold">{company}</h3>
+                <p className="text-xl text-indigo-400 font-medium">
+                  {position}
+                </p>
+                <p className="text-gray-400 text-sm">
+                  {period} | {location}
+                </p>
+                <p className="mt-4 text-gray-300">{summary}</p>
+              </div>
+            )
+          )}
+        </div>
       </div>
     </div>
   );
